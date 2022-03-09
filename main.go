4d80a80a50147392ce5bded4b8597f0392b1ff3d
kvothe838 <facundo@omniasalud.com>
2022-03-08 22:03:04 -0300

    feat(main): added endpoint to get total distinct visitors by url

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/gorilla/mux"
 )
@@ -16,6 +17,7 @@ func main() {
 func handleRequests() {
 	myRouter := mux.NewRouter()
 	myRouter.HandleFunc("/visit", visitUrl).Methods("POST")
+	myRouter.HandleFunc("/getVisitors", getVisitors).Methods("GET")
 	log.Println("serving in port 10000")
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
@@ -37,8 +39,53 @@ func visitUrl(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 
+	log.Println("new decode")
+	log.Printf("id: %s", data.VisitorId)
+	log.Printf("url: %s", data.VisitedUrl)
+
 	visits = append(visits, data)
 	Json(response, http.StatusOK, nil)
+}
+
+func getVisitors(response http.ResponseWriter, request *http.Request) {
+	urlParam := request.FormValue("url")
+
+	if len(urlParam) == 0 {
+		log.Printf("empty url read")
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	url, err := url.QueryUnescape(urlParam)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	distinctVisitors := make(map[string]bool, 0)
+
+	for _, visit := range visits {
+		log.Printf("visit.VisitedUrl: %s", visit.VisitedUrl)
+		if visit.VisitedUrl == url {
+			distinctVisitors[visit.VisitorId] = true
+		}
+	}
+
+	totalVisitors := 0
+
+	for _, hasVisitedUrl := range distinctVisitors {
+		if hasVisitedUrl {
+			totalVisitors++
+		}
+	}
+
+	responseData := struct {
+		TotalVisitors int
+	}{
+		TotalVisitors: totalVisitors,
+	}
+
+	Json(response, http.StatusOK, responseData)
 }
 
 func decode(reader io.Reader, val interface{}) error {
